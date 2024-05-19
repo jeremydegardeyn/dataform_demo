2d0d7b30417a594c296d92634153acc841fc7bf4
jsdegard <jeremy@datadinosaur.com>
2024-05-19 15:25:13 +0000

    init

---
+++ includes/constants.js
@@ -0,0 +1,6 @@
+const TRG_PROJECT_ID = 'strongsville-city-schools';
+const SRC_PROJECT_ID = "bigquery-public-data";
+const DATASET = 'dataform';
+const DQ_DATASET = "breathe";
+
+module.exports = { TRG_PROJECT_ID , DATASET , SRC_PROJECT_ID , DQ_DATASET };
