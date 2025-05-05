cc4dbf4c1643a4214ed7a7e4e2e7f7461e906df0
jsdegard <jeremy@datadinosaur.com>
2025-05-05 13:40:03 +0000

    added testing

---
+++ includes/policy_tags.js
@@ -0,0 +1,8 @@
+const datagovProject = "strongsville-city-schools"
+const taxonomy = "8678378231237580896"
+const policy_tag01 = "projects/" + datagovProject + "/locations/us/taxonomies/" + taxonomy + "/policyTags/8681289058619599169"
+
+module.exports = { 
+    datagovProject, 
+    policy_tag01 
+};
