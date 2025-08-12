458ccc17002399860c92c489bfa78efcd96f6946
jsdegard <jeremy@datadinosaur.com>
2025-08-12 16:09:22 +0000

    dksdksd

--- includes/policy_tags.js
+++ includes/policy_tags.js
@@ -1,8 +1,15 @@
 const datagovProject = "strongsville-city-schools"
 const taxonomy = "8678378231237580896"
 const policy_tag01 = "projects/" + datagovProject + "/locations/us/taxonomies/" + taxonomy + "/policyTags/8681289058619599169"
-
+const policy_tag02 = "projects/strongsville-city-schools/locations/us/taxonomies/7693680332137469110/policyTags/1939754752863472770"
+const policy_tag03 = "projects/strongsville-city-schools/locations/us/taxonomies/8678378231237580896/policyTags/5641304534866435701"
+const policy_tag04 = "projects/strongsville-city-schools/locations/us/taxonomies/8678378231237580896/policyTags/3363490109226205199"
+const policy_tag05 = "projects/strongsville-city-schools/locations/us/taxonomies/8678378231237580896/policyTags/7960051088540944825"
 module.exports = { 
     datagovProject, 
-    policy_tag01 
+    policy_tag01,
+    policy_tag02,
+    policy_tag03,
+    policy_tag04,
+    policy_tag05
 };
